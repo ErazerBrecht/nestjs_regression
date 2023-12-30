6d073e264dcff4efbacd3f1faaf74fbe5873a8c0
Brecht Carlier <brecht@intigriti.com>
2023-12-30 13:17:02 +0100

    Repro of a regression in NestJS

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import * as fs from 'fs';
+
+
+async function bootstrap() {
+  const config = JSON.parse(fs.readFileSync(join(__dirname, 'appsettings.json'), 'utf8'));
+  console.log(config.motd);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
